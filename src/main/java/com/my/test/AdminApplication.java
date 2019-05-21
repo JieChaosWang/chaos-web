4ab9f46abfacd3a2343d2ef77c575f3f618c8fa1
wangchaojie <wangchaojie@beecredit.com>
2019-05-21 13:53:09 +0800

    统计管理,跳转不了正确页面

--- src/main/java/com/my/test/AdminApplication.java
+++ src/main/java/com/my/test/AdminApplication.java
@@ -1,12 +1,10 @@
 package com.my.test;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-//@MapperScan("com.my.test.mapper")
 @EnableTransactionManagement
 public class AdminApplication {
 
--- src/main/java/com/my/test/controller/StatisticsInfoController.java
+++ src/main/java/com/my/test/controller/StatisticsInfoController.java
@@ -1,14 +1,9 @@
 package com.my.test.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.my.test.pojo.Authority;
 import com.my.test.pojo.StatisticsInfo;
-import com.my.test.pojo.User;
 import com.my.test.service.StatisticsInfoService;
-import com.my.test.service.UserService;
-import com.my.test.service.impl.Pagination;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.sql.Date;
 import java.util.List;
 
@@ -32,7 +26,7 @@ import java.util.List;
  * ********************************************************.<br>
  */
 @RestController
-@RequestMapping("/statisticsInfo")
+@RequestMapping(value = "/statisticsInfo")
 public class StatisticsInfoController {
 
     protected static Logger logger = LoggerFactory.getLogger(BaseController.class);
@@ -51,10 +45,10 @@ public class StatisticsInfoController {
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
-        return "statisticsInfo/list";
+        return "statistics_info/list";
     }
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @RequestMapping(value = "/queryStatisticsInfoList", method = RequestMethod.GET)
     public String queryStatisticsInfoList(StatisticsInfo statisticsInfo, PageInfo pageInfo, Model model) {
 
 
@@ -65,6 +59,6 @@ public class StatisticsInfoController {
             model.addAttribute("page", statistics);
 
 
-        return "statisticsInfo/list";
+        return "statistics_info/list1";
     }
 }
